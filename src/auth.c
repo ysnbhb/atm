c1d8ef17dbc9381f823cb7b990949c850a579af9
ysnbhb <yassine.bahbib@usmba.ac.ma>
2024-09-07 16:11:29 +0100

    ok

---
+++ src/auth.c
@@ -0,0 +1,98 @@
+#include <termios.h>
+#include "header.h"
+
+char *USERS = "./data/users.txt";
+
+void loginMenu(char a[50], char pass[50])
+{
+    struct termios oflags, nflags;
+
+    system("clear");
+    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Login:");
+    scanf("%s", a);
+
+    // disabling echo
+    tcgetattr(fileno(stdin), &oflags);
+    nflags = oflags;
+    nflags.c_lflag &= ~ECHO;
+    nflags.c_lflag |= ECHONL;
+
+    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
+    {
+        perror("tcsetattr");
+        return exit(1);
+    }
+    printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
+    scanf("%s", pass);
+
+    // restore terminal
+    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
+    {
+        perror("tcsetattr");
+        return exit(1);
+    }
+};
+
+int getPassword(struct User u)
+{
+    FILE *fp;
+    struct User userChecker;
+
+    if ((fp = fopen("./data/users.txt", "r")) == NULL)
+    {
+        printf("Error! opening file");
+        exit(1);
+    }
+    int i = 0;
+    while (fscanf(fp, "%d %s %s", &userChecker.id, userChecker.name, userChecker.password) != EOF)
+    {
+        if (strcmp(userChecker.name, u.name) == 0 && strcmp(userChecker.password, u.password) == 0)
+        {
+            fclose(fp);
+            return 1;
+        }
+    }
+
+    fclose(fp);
+    return 0;
+}
+
+void Rejestre(struct User *user)
+{
+    struct termios oflags, nflags;
+    char pass[50];
+    printf("\n\t\t\t\tUser Login \n");
+    scanf("%s", user->name);
+notSame:
+    tcgetattr(fileno(stdin), &oflags);
+    nflags = oflags;
+    nflags.c_lflag &= ~ECHO;
+    nflags.c_lflag |= ECHONL;
+
+    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
+    {
+        perror("tcsetattr");
+        return exit(1);
+    }
+    printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
+    scanf("%s", user->password);
+
+    printf("\n\n\n\n\n\t\t\t\tEnter the password to login again :");
+    scanf("%s", pass);
+    if (strcmp(pass, user->password) != 0)
+    {
+        printf("not the same please try again : \n");
+        if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
+        {
+            perror("tcsetattr");
+            return exit(1);
+        }
+        goto notSame;
+    }
+    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
+    {
+        perror("tcsetattr");
+        return exit(1);
+    }
+    createNewAcc(*user);
+}
---
+++ src/main.c
@@ -0,0 +1,111 @@
+#include "header.h"
+
+void mainMenu(struct User u)
+{
+    system("clear");
+    int option;
+    printf("\n\n\t\t======= ATM =======\n\n");
+    printf("\n\t\t-->> Feel free to choose one of the options below <<--\n");
+    printf("\n\t\t[1]- Create a new account\n");
+    printf("\n\t\t[2]- Update account information\n");
+    printf("\n\t\t[3]- Check accounts\n");
+    printf("\n\t\t[4]- Check list of owned account\n");
+    printf("\n\t\t[5]- Make Transaction\n");
+    printf("\n\t\t[6]- Remove existing account\n");
+    printf("\n\t\t[7]- Transfer ownership\n");
+    printf("\n\t\t[8]- Exit\n");
+chois:
+    if (scanf("%d", &option) != 1)
+        clear();
+
+    switch (option)
+    {
+    case 1:
+        createNewAcc(u);
+        break;
+    case 2:
+        // student TODO : add your **Update account information** function
+        // here
+        break;
+    case 3:
+        // student TODO : add your **Check the details of existing accounts** function
+        // here
+        break;
+    case 4:
+        checkAllAccounts(u);
+        break;
+    case 5:
+        // student TODO : add your **Make transaction** function
+        // here
+        break;
+    case 6:
+        // student TODO : add your **Remove existing account** function
+        // here
+        break;
+    case 7:
+        // student TODO : add your **Transfer owner** function
+        // here
+        break;
+    case 8:
+        exit(1);
+        break;
+    default:
+        printf("Invalid operation!\n");
+        goto chois;
+    }
+};
+
+void initMenu(struct User *u)
+{
+    int r = 0;
+    int option;
+    system("clear");
+    printf("\n\n\t\t======= ATM =======\n");
+    printf("\n\t\t-->> Feel free to login / register :\n");
+    printf("\n\t\t[1]- login\n");
+    printf("\n\t\t[2]- register\n");
+    printf("\n\t\t[3]- exit\n");
+    while (!r)
+    {
+        if (scanf("%d", &option) != 1)
+            clear();
+
+        switch (option)
+        {
+        case 1:
+            loginMenu(u->name, u->password);
+            // printf("%s %s",u->name , u->password);
+            if (getPassword(*u))
+            {
+                printf("\n\nPassword Match!");
+            }
+            else
+            {
+                printf("\nWrong password!! or User Name\n");
+                exit(1);
+            }
+            r = 1;
+            break;
+        case 2:
+            Rejestre(u);
+            // student TODO : add your **Registration** function
+            // here
+            r = 1;
+            break;
+        case 3:
+            exit(1);
+            break;
+        default:
+            printf("Insert a valid operation!\n");
+        }
+    }
+};
+
+int main()
+{
+    struct User u;
+
+    initMenu(&u);
+    mainMenu(u);
+    return 0;
+}
